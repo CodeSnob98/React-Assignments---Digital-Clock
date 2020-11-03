f3fff6f226dd51f8dd8fbef066357a1196061ba6
CodeSnob98 <63289568+CodeSnob98@users.noreply.github.com>
2020-11-03 15:23:57 +0530

    changes1

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,33 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            time:null
+        };
+        this.ctrlID=null;
+    }
+
+    comentDidMount(){
+        this.ctrlID=setInterval(function () {
+            let  event = new Date();
+            this.setState({time:event.toLocaleTimeString('en-US')});
+          }, 1000);
+    }
+    componentWillUnmount(){
+    }
     render() {
 
         return(
             <>
-               
+              <div className="Clock">
+                  <h3 id="time">
+                      {this.state.time}
+                  </h3>
+              </div>
+
             </>
         )
     }
