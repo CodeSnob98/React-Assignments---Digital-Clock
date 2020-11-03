9dc7d3c4d3a3abfa3513a98a9d2871e5f43f420e
CodeSnob98 <63289568+CodeSnob98@users.noreply.github.com>
2020-11-03 16:06:46 +0530

    changes6

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,41 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      time: ""
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
     };
-    this.ctrlID = setInterval(function () {
-      let event = new Date();
-      this.setState({ time: event.toLocaleTimeString("en-US") });
-    }, 1000);
+  }
+  tick() {
+    this.setState({
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
+    });
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000);
   }
   componentWillUnmount() {
-    clearInterval(this.ctrlID);
+    clearInterval(this.intervalID);
   }
   render() {
     return (
-      <>
+      <div className="App">
         <div className="Clock">
-          <h3 id="time">{this.state.time}</h3>
+          <h3 id="time">{`${this.state.time}`}</h3>
         </div>
-      </>
+      </div>
     );
   }
 }
