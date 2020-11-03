60584d8ca0276c7302f7dc809390cceb02720c11
CodeSnob98 <63289568+CodeSnob98@users.noreply.github.com>
2020-11-03 15:57:07 +0530

    changes5

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     this.ctrlID = null;
   }
 
-  comentDidMount() {
+  componentDidMount() {
     this.ctrlID = setInterval(function () {
       let event = new Date();
       this.setState({ time: event.toLocaleTimeString("en-US") });
